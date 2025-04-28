0bde3f3e7f0e3ca511696a888fbe6cb482f08730
Manikandan <146983913+Maniboomi@users.noreply.github.com>
2025-04-28 17:13:14 +0530

    Create Sizeof.c

---
+++ Day_02/Sizeof.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("Size of int:%zubytes\n",sizeof(int));
+	printf("Size of char:%zubytes\n",sizeof(char));
+	printf("Size of float:%zubytes\n",sizeof(float));
+	printf("Size of double:%zubyltes\n",sizeof(double));
+	return 0;
+}
