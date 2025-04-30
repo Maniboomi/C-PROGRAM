f636b9b09bf9844a54cea211b1a27ae42f9e51fc
Manikandan <146983913+Maniboomi@users.noreply.github.com>
2025-04-30 11:12:20 +0530

    Update power_of_number.c

--- Day_05/power_of_number.c
+++ Day_05/power_of_number.c
@@ -1,1 +1,20 @@
-_
+#include<stdio.h>
+int  power(int n1, int n2);
+int main()
+{
+	int base,a,result;
+	printf("Enter the base value: ");
+	scanf("%d",&base);
+	printf("Enter the exponent value: ");
+	scanf("%d",&a);
+	result = power(base,a);
+	printf("The result is %d^%d = %d\n",base,a,result);
+	return 0;
+}
+int power(int base, int a)
+{
+	if(a !=0)
+		return(base * power(base , a - 1));
+	else
+		return 1;
+}
