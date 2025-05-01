cc26604e1a26ac612f149d20027482f8c9d17315
Manikandan <146983913+Maniboomi@users.noreply.github.com>
2025-05-01 16:13:09 +0530

    Create Struct_Pointer.c

---
+++ Day_05/Struct_Pointer.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	struct student
+	{
+		char name[50];
+		int rollno;
+		int mark;
+	};
+	struct student s1;
+	struct student *p;
+	p = &s1;
+	printf("Enter name, rollno, mark: ");
+	scanf("%s %d %d",&s1.name, &s1.rollno, &s1.mark);
+        printf("NAME : %s\n",p->name);
+	printf("ROLLNO : %d\n",p->rollno);
+	printf("MARK : %d\n",p->mark);
+	return 0;
+}
+
