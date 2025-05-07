16ff29ed49d0a505acb08743b5d70b1545803399
Manikandan <146983913+Maniboomi@users.noreply.github.com>
2025-05-07 15:17:40 +0530

    Update Example4.c

--- Day_07/Pattern_Printing/Example4.c
+++ Day_07/Pattern_Printing/Example4.c
@@ -2,12 +2,12 @@
 #include<stdio.h>
 int main()
 	{
-	int i,j,n;
+	int i,j;
 	for (i=1; i<=7; i++)
 	{
 		for (j=1; j<=7; j++)
 		{
-			if(i==1 || i==7 || j==1 || j==7 || i==j || j==(n-i+1))
+			if(i==1 || i==7 || j==1 || j==7 || i==j || j==(7-i+1))
 			{			
 				printf("*");
 			}
