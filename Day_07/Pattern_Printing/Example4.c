f65130a833abe96a102199c1a39e7d7588507675
Manikandan <146983913+Maniboomi@users.noreply.github.com>
2025-05-07 14:34:01 +0530

    Create Example4.c

---
+++ Day_07/Pattern_Printing/Example4.c
@@ -0,0 +1,20 @@
+//HALLOW SQUARE WITH DIAGONAL
+#include<stdio.h>
+int main()
+	{
+	int i,j,n;
+	for (i=1; i<=7; i++)
+	{
+		for (j=1; j<=7; j++)
+		{
+			if(i==1 || i==7 || j==1 || j==7 || i==j || j==(n-i+1))
+			{			
+				printf("*");
+			}
+			else
+				printf(" ");
+		}
+		printf("\n");
+	}
+	printf("\n");
+	}
