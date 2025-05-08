64584c0aabf1e72ea243ed01e8a90abc35621824
Manikandan <146983913+Maniboomi@users.noreply.github.com>
2025-05-08 10:38:29 +0530

    Update Example7.c

--- Day_07/Pattern_Printing/Example7.c
+++ Day_07/Pattern_Printing/Example7.c
@@ -1,1 +1,21 @@
-
+//MIRRORED RHOMBUS STAR PATTERN
+#include<stdio.h>
+int main()
+{
+	int i,j,n;
+	printf("Enter No of Rows: ");
+	scanf("%d",&n);
+	for (i=0; i<=n; i++)
+	{
+		for (j=0; j<i; j++)
+		{
+			printf(" ");
+		}
+		for (j=0; j<=n; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	printf("\n");
+}
