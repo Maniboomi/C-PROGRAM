b730b803aa57a2dd01de80b9a671e349d1a9ccc1
Manikandan <146983913+Maniboomi@users.noreply.github.com>
2025-05-14 14:54:21 +0530

    Update Example_19.c

--- Day_07/Pattern_Printing/Example_19.c
+++ Day_07/Pattern_Printing/Example_19.c
@@ -1,1 +1,42 @@
-
+//PLUS IN STAR PATTERN
+//PLUS STAR PATTERN
+#include<stdio.h>
+int main()
+{
+	int i,j,n;
+	printf("Enter No of Rows: ");
+	scanf("%d",&n);
+	for (i=1;i<n;i++)
+	{
+		for(j=1;j<=n-1;j++)
+		{
+			printf(" ");
+		}
+		for (j=1;j<=1;j++)
+		{
+			printf("+");
+		}
+		printf("\n");
+	}
+	for (i=1;i<=1;i++)
+	{
+		for(j=1;j<=n*2-1;j++)
+		{
+			printf("+");
+		}
+		printf("\n");
+	}
+	for (i=n-1;i>=1;i--)
+	{
+		for (j=1;j<=n-1;j++)
+		{
+			printf(" ");
+		}
+		for (j=1;j<=1;j++)
+		{
+			printf("+");
+		}
+		printf("\n");
+	}
+	printf("\n");
+}
