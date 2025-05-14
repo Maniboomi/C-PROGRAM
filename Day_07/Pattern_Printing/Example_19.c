b60b7a977bae9c74ae2fc8bad3c17784c5cce831
Manikandan <146983913+Maniboomi@users.noreply.github.com>
2025-05-14 10:05:52 +0530

    Create Example_19.c

---
+++ Day_07/Pattern_Printing/Example_19.c
@@ -0,0 +1,33 @@
+//RIGHT ARROW STAR PATTERN
+#include<stdio.h>
+int main()
+{
+	int i,j,n;
+	printf("Enter No of Rows: ");
+	scanf("%d",&n);
+	for (i=1;i<=n;i++)
+	{
+		for(j=1;j<=i*2-2;j++)
+		{
+			printf(" ");
+		}
+		for (j=1;j<=n-i+1;j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	for (i=n-1;i>=1;i--)
+	{
+		for (j=1;j<=i*2-2;j++)
+		{
+			printf(" ");
+		}
+		for (j=1;j<=n-i+1;j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	printf("\n");
+}
