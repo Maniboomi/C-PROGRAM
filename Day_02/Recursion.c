9da362c3b8cc7ecd715f0fd9a3dbb15d0956f18f
Manikandan <146983913+Maniboomi@users.noreply.github.com>
2025-05-30 11:43:07 +0530

    Update Recursion.c

--- Day_02/Recursion.c
+++ Day_02/Recursion.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
-int main()
-{
-	int n;
-	printf("Enter a Number: ");
-	scanf("%d",&n);
-	int fact(int n);
+int fact(int n)
 	{
 		if(n==0)
 			return 1;
 		else
 			return n*fact(n-1);
 	}
+int main()
+{
+	int n;
+	printf("Enter a Number: ");
+	scanf("%d",&n);
 	printf("Factorial is %d\n",fact(n));
 }	
