7be1049af12a7260599ccc0052662abec42738d6
Manikandan <146983913+Maniboomi@users.noreply.github.com>
2025-06-03 10:13:03 +0530

    Create 13_Assign.c

---
+++ Day_08/13_Assign.c
@@ -0,0 +1,10 @@
+//SIGNED_UNSIGNED
+#include<stdio.h>
+int main()
+{
+	signed int signed_num = -10;
+	unsigned int unsigned_num = -10;
+	printf("signed integer value : %d\n",signed_num);
+	printf("unsigned integer value : %u\n",unsigned_num);
+	return 0;
+}
