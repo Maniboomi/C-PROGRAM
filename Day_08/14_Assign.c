918e1eb813609b364dfe0e896abdde1c5b6896e9
Manikandan <146983913+Maniboomi@users.noreply.github.com>
2025-06-03 10:18:39 +0530

    Create 14_Assign.c

---
+++ Day_08/14_Assign.c
@@ -0,0 +1,14 @@
+//ASCII_ADDITION
+#include<stdio.h>
+int main()
+{
+	char ch = 'a';
+	char cd = 'b';
+	int total;
+        total = ch + cd;
+	printf("the ascii value of a is: %d\n",ch);
+	printf("the ascii value of b is: %d\n",cd);
+	printf("the ascii value of a+b is: %d\n",total);
+}
+
+
