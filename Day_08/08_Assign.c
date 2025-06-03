388b2f04c9dc2bf45248e74c06b55af8fa490d4c
Manikandan <146983913+Maniboomi@users.noreply.github.com>
2025-06-03 10:06:31 +0530

    Create 08_Assign.c

---
+++ Day_08/08_Assign.c
@@ -0,0 +1,16 @@
+//STUDENT DETAILS
+#include<stdio.h>
+int main()
+{
+	long int rollnumber = 822121104019;
+	float attendence = 78.5;
+	char department[] = "CSE";
+	double cgpa = 7.89;
+	long int batch = 2025;
+	printf("ROLLNUMBER : %ld\n",rollnumber);
+	printf("ATTENDENCE : %.2f\n",attendence);
+	printf("DEPARTMENT : %s\n",department);
+	printf("CGPA : %.2lf\n",cgpa);
+	printf("BATCH : %ld\n",batch);
+	return 0;
+}
