8725cffdebeab32e6da437e3be41ebcec94eca07
Manikandan <146983913+Maniboomi@users.noreply.github.com>
2025-06-03 10:42:06 +0530

    Create 21_Assign.c

---
+++ Day_08/21_Assign.c
@@ -0,0 +1,14 @@
+//SQUARE NUMBER
+#include<stdio.h>
+int main()
+{
+	int number = 7;
+	int square;
+	int data;
+	square = number * number;
+	data = square;
+	printf("Enter the number value : %d\n",number);
+	printf("Enter the square value : %d\n",square);
+	printf("the changed variable value : %d\n",data);
+}
+
