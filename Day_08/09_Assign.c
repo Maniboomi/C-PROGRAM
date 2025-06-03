44c0ad405ad2f728b2d297e371e83b203e7987f0
Manikandan <146983913+Maniboomi@users.noreply.github.com>
2025-06-03 10:09:23 +0530

    Create 09_Assign.c

---
+++ Day_08/09_Assign.c
@@ -0,0 +1,16 @@
+//SIZEOF_OPERATOR
+#include<stdio.h>
+
+int main()
+{
+	printf("Size of int:%zubytes\n",sizeof(int));
+	printf("Size of char:%zubytes\n",sizeof(char));
+	printf("Size of float:%zubytes\n",sizeof(float));
+	printf("Size of double:%zubyltes\n",sizeof(double));
+	printf("Size of long int:%zubyltes\n",sizeof(long int));
+	printf("Size of short int:%zubyltes\n",sizeof(short int));
+	printf("Size of unsigned int:%zubyltes\n",sizeof(unsigned int));
+	printf("Size of signed int:%zubyltes\n",sizeof(signed int));
+	printf("Size of long long int:%zubyltes\n",sizeof(long long int));
+	return 0;
+}
