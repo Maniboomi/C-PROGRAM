c551de7ba5123ca62bbc1da70b0fed29e84c5e0e
Manikandan <146983913+Maniboomi@users.noreply.github.com>
2025-06-03 10:46:53 +0530

    Create 22_Assign.c

---
+++ Day_08/22_Assign.c
@@ -0,0 +1,17 @@
+//ACCEPT USER-INPUT FOR THREE VARIABLES
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("Enter the first number : \n");
+	scanf("%d",&a);
+	printf("Enter the second number : \n");
+	scanf("%d",&b);
+	printf("Enter the third number : \n");
+	scanf("%d",&c);
+	printf(" the first variable : %d\n",a);
+	printf(" the second variable : %d\n",b);
+	printf(" the third variable : %d\n",c);
+  return 0;
+}
+
