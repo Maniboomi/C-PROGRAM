b6527edeb2ba971c6addad2f77f47f9fe9af8965
Manikandan <146983913+Maniboomi@users.noreply.github.com>
2025-06-03 10:57:17 +0530

    Create 24_Assign.c

---
+++ Day_08/24_Assign.c
@@ -0,0 +1,13 @@
+//AREA OF CIRCLE 
+#include<stdio.h>
+int main()
+{
+	const float pie = 3.14;
+	int radius;
+	float area;
+	printf("Enter the radius : ");
+	scanf("%d",&radius);
+	area = (float)(pie *radius *radius);
+	printf("area of a circle is : %.2f\n",area);
+	return 0;
+}
