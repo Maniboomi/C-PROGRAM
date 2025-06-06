87ee7f1dd6b2b63d8178a4d2273248ce793d1a5e
Manikandan <146983913+Maniboomi@users.noreply.github.com>
2025-06-06 15:34:24 +0530

    Update 02_QUICKSORT.c

--- Day_09/02_QUICKSORT.c
+++ Day_09/02_QUICKSORT.c
@@ -1,1 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+int compare(const int* a, const int* b)
+{
+	return (*a - *b);
+}
+int main()
+{
+	int i, n;
+	int a[] = {4,2,6,8,3};
+	n = sizeof(a)/sizeof(a[0]);
+	qsort(a,n,sizeof(int),compare);
+	for(i=0;i<n;i++)
+	printf("%d",a[i]);
+	printf("\n");
+	return 0;
+}
+
 
